aa4cb554a477c5251b08ff935525241912cfb912
srinivasan <srinivasan.gopalakrishnan@charter.com>
2017-12-11 07:24:01 -0700

    Login Model

---
+++ Auction/LoginModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoginModel.swift
+//  Auction Login
+//
+//  Created by Cognizant on 11/21/17.
+//  Copyright © 2017 Cognizant. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginModel: UIView {
+
+    // data Model test
+    class loginAuthentication {
+        var userName: String?
+        var password: String?
+    }
+}
